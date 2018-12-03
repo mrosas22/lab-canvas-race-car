b6e92f9478dfea0207a8748591b713b4e50d3b16
mrosas22 <miller.rosas22@gmail.com>
2018-12-03 14:51:15 -0500

    Finish logic, need to add Images

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,85 +3,92 @@ window.onload = function() {
     startGame();
   };
     //create a Obstacles array to store all of our obstacles
-    let myObstacles = [];
-  function startGame() {
+  let myObstacles = [];
+  // const carImage = new Image();
+  // carImage.src = './images/car.png';
+function startGame() {
     myBoard.start();
-    car = new component (30, 30, 'green', 185, 1150)
-  }
+    // car = new component (carImage, 30, 30, 185, 450)
+    car = new component (30, 30, 'green', 185, 450)
+}
 
-  let myBoard = {
+let myBoard = {
     canvas: document.createElement('canvas'),
     start: function (){
-      this.canvas.width = 400;
-      this.canvas.height = 1200;
-      this.context = this.canvas.getContext('2d');
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      //schedule updates
+        this.canvas.width = 400;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(this.canvas, document.body.childNodes[1]);
+        //schedule updates
       this.interval = setInterval(updateMyBoard, 20);
     },
     frames: 0, //to count how many times we update our board
     clear: function(){
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    score: function(){
-      points = (Math.floor(this.frames/240))
-      this.context.fillStyle = 'Black'
-      this.context.font = '30 Arial';
-      this.context.fillText(`Score: ${points}`, 350, 50)
-    },
-    stop: function(){
-      clearInterval(this.interval);
-    }
-  
+          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      },
+  score: function(){
+    points = (Math.floor(this.frames/120))
+    this.context.font = '30 Arial';
+    this.context.fillStyle = 'Black';
+    this.context.fillText(`Score: ${points}`, 350, 50);
+  },
+  stop: function(){
+        clearInterval(this.interval);
+  },
+  gameOver: function (){
+    this.context.font = 'bold 40px Arial';
+    this.context.fillStyle = 'red';
+    this.context.fillText('Game Over ', 80, 120)
   }
+  
+}
 
-  function component (width, height, color, x, y){
+function component (width, height, color, x, y){
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
     this.speedX = 0;
     this.speedY = 0;
+    // this.image = image;
     this.update = function (){
-      ctx = myBoard.context;
-      // ctx.drawImage(this.image, this.x, this.y, 50, 50);
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx = myBoard.context;
+        // let image = new Image();
+        // image.src = './images/car.png';
+        // ctx.drawImage(this.image,this.x, this.y, this.width, this.height);
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
     this.newPos = function(){
-      this.x += this.speedX;
-      this.y += this.speedY;
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
-    this.left = function (){return this.y}
-    this.right = function (){return (this.y + this.height)}
-    this.top = function (){ return this.x}
-    this.bottom = function (){return this.x + (this.width)}
+    this.left   = function (){ return this.x                }
+    this.right  = function (){ return (this.x + this.width)}
+    this.top    = function (){ return this.y                }
+    this.bottom = function (){ return this.y  + (this.height)}
     //= = = = = = = = = CRASH = = = = = = = = = = = = 
-    this.crash = function(obstacle){
-      return !((this.bottom() < obstacle.top())     ||
-              (this.top()     > obstacle.bottom())  ||
-              (this.right()   < obstacle.left())    ||
-              (this.left()    > obstacle.right()))
+    this.crashWith = function(obstacle){
+      return !((this.bottom()  < obstacle.top())    ||
+               (this.top()     > obstacle.bottom()) ||
+               (this.right()   < obstacle.left())   ||
+               (this.left()    > obstacle.right()))
     }
 
-  }
-  function updateMyBoard(){
-    // let crashed = myObstacles.some(function(obstacle){
-    //   return player.crash(obstacle)
-    // })
-    // if (crashed){
-    //   myBoard.stop();
-    // }
+}
+
+function updateMyBoard(){
     //check if car crashes with obstacles
-    // for (i =0; i< myObstacles.length; i += 1){
-    //   if (player.crash(myObstacles[i])){
-    //     myBoard.stop();
-    //     return;
-    //   }
-    // }
-        myBoard.clear();
-        myBoard.frames += 1; //Everytime we update our game we will add 1 Frame 
-        if( myBoard.frames % 180 === 0){
+    for (i =0; i< myObstacles.length; i += 1){
+      if (car.crashWith(myObstacles[i])){
+        myBoard.stop();
+        myBoard.gameOver();
+        return;
+      }
+    }
+    myBoard.clear();
+    myBoard.frames += 1; //Everytime we update our game we will add 1 Frame 
+      if( myBoard.frames % 120 === 0){
           y = myBoard.canvas.height; //set the obtacles according to our y coordinates
           //get a random value for our obstacle width
           minWidth = 120;
@@ -107,10 +114,10 @@ window.onload = function() {
   }
 
   function moveUp(){
-    car.speedY += 1;
+    car.speedY -= 1;
   }
   function moveDown (){
-    player.speedY -= 1;
+    car.speedY += 1;
   }
   function moveLeft (){
     car.speedX -= 1;
@@ -118,24 +125,24 @@ window.onload = function() {
   function moveRight (){
     car.speedX += 1;
   }
-  document.onKeydown = function (e){
+  document.onkeydown = function (e){
     switch (e.keyCode){
-      case 38 :
-      moveUp();
-      break;
-      case 40:
+      case 37 : //left
+        moveLeft();
+        break;
+      case 39: //right
+        moveRight();
+        break;
+      case 38: //up
+        moveUp();
+        break;
+      case 40: //down
       moveDown();
-      break;
-      case 37:
-      moveLeft();
-      break;
-      case 39:
-      moveRight();
       break;
     }
   }
 
-  document.onkeyup =  function (){
+  document.onkeyup =  function (e){
     stopMove();
   }
   function stopMove(){
